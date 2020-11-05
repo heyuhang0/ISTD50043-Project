36f02795e53b940b87e59c59e832cde62105dc20
Jiazheng411 <644839468@qq.com>
2020-11-05 21:33:50 +0800

    add category apis

--- src/api/controllers/categoryController.js
+++ src/api/controllers/categoryController.js
@@ -1,13 +1,27 @@
-mockdata = require('../helpers/mockdata');
 
-var mockCategories = mockdata.mockCategories;
+var Category = require('../models/category');
+mockdata = require('../helpers/mockdata');
 
 exports.all_categories_get = function(req, res){
     console.log("getting all categories");
-    res.json({categories: mockCategories});
-}
+    Category.find()
+      .populate('categories')
+      .exec(function (err, list_genres) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.json({category_list: list_genres});
+      });
+    }
 
 exports.suggested_categories_get = function(req, res){
     console.log("getting suggested categories");
-    res.json({categories: mockCategories});
-}
+    Category.find()
+      .populate('categories')
+      .sort({book_count:-1})
+      .limit(10)
+      .exec(function (err, list_genres) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.json({category_list: list_genres});
+      });
+    }
