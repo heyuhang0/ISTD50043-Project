546679d5adf30707d660ab5448a4ea555859132b
Wang Zilin <zilin_wang@mymail.sutd.edu.sg>
2020-11-07 23:14:17 +0800

    update functions, generating ASIN number unresolved

--- src/api/controllers/bookController.js
+++ src/api/controllers/bookController.js
@@ -13,11 +13,35 @@ exports.book_search_get = function (req, res) {
     let offset = Number(req.query.offset) || 0;
     var ASINJson;
     console.log(
-<<<<<<< HEAD
       'Searching for books with',
       'keyword=' + keyword,
       'limit=' + limit,
       'offset=' + offset);
+    
+    // > 100 per page
+    if (limit > 100) {
+    res.status(400)
+        .send({
+            success: 0,
+            error_type: 1,
+            error_msg: "limit must less than 100"
+        });
+    return;
+    };
+
+    // > 1000 records
+    if (offset + limit > 1000) {
+        console.log((offset + 1) * limit);
+        console.log(offset, limit)
+        res.status(400)
+            .send({
+                success: 0,
+                error_type: 2,
+                error_msg: "exceeding max search limit",
+            });
+        return;
+    };
+
     if(!keyword){
         res.json({message: "Please enter your keywords!"});
         console.log("Empty keyword!");
@@ -30,7 +54,7 @@ exports.book_search_get = function (req, res) {
             if(err){
                 res.status(400).send({
                     success: 0,
-                    error_type: 2,
+                    error_type: 3,
                     error_msg: "Error during finding book with ASIN"
                 });
                 return;
@@ -39,22 +63,8 @@ exports.book_search_get = function (req, res) {
                 console.log("1 result found with ASIN!");
             }
             ASINJson = book;
-=======
-        'Searching for books with',
-        'keyword=' + keyword,
-        'limit=' + limit,
-        'offset=' + offset);
-    var list_of_keyword = keyword.split(" ");
-    var keywords_for_find = new Array();
-    // 1. match asin; 2. title/author
-    if (keyword.length == 10 && keyword.charAt(0) === 'B') {
-        Book.findOne({ "asin": keyword }, function (err, book) {
-            console.log("1 result found with ASIN!");
-            res.json(book);
->>>>>>> zilin_api_spec
         });
     }
-<<<<<<< HEAD
     var list_of_keyword = keyword.split(" ");
     var keywords_for_find = new Array();
     list_of_keyword.forEach(function(value, index, array){
@@ -68,7 +78,7 @@ exports.book_search_get = function (req, res) {
         if(err){
             res.status(400).send({
                 success: 0,
-                error_type: 2,
+                error_type: 3,
                 error_msg: "Error during finding book with title/author"
             });
             return;
@@ -83,17 +93,6 @@ exports.book_search_get = function (req, res) {
             res.json({message: "No result found!"});
         }
         
-=======
-    list_of_keyword.forEach(function (value, index, array) {
-        keywords_for_find.push({
-            "$or": [{ 'author': { "$regex": " " + value, "$options": "i" } }, { 'author': { "$regex": value + " ", "$options": "i" } },
-            { 'title': { "$regex": " " + value, "$options": "i" } }, { 'title': { "$regex": value + " ", "$options": "i" } }]
-        });
-    });
-    Book.find({ "$and": keywords_for_find }).limit(parseInt(limit)).skip(parseInt(limit) * parseInt(offset)).exec(function (err, books) {
-        console.log(books.length + " result(s) found!");
-        res.json(books);
->>>>>>> zilin_api_spec
     });
     // res.json(mockBooks);
 };
@@ -103,16 +102,22 @@ exports.book_find_by_price = function (req, res) {
     var price = parseFloat(req.query.price);
     var q = Book.find({ 'price': price }).limit(5);
     q.exec(function (err, book) {
+        if(err){
+            res.status(400).send({
+                success: 0,
+                error_type: 3,
+                error_msg: "Error during finding book by price"
+            });
+            return;
+        }
         res.json(book);
-        console.log(book);
         console.log(book.length + " result(s) found!");
     });
 }
 
 // Trending books
-<<<<<<< HEAD
 exports.book_trending_get = function(req,res) {
-    Book.find({}).limit(5).sort({review_number: -1}).exec(function(err, books){
+    Book.find({}).limit(5).sort([["review_number", -1], ["rating_average", -1]]).exec(function(err, books){
         if(err){
             res.status(400).send({
                 success: 0,
@@ -123,10 +128,6 @@ exports.book_trending_get = function(req,res) {
         }
         res.json(books);
     });
-=======
-exports.book_trending_get = function (req, res) {
-    res.json({ 'books': mockBooks });
->>>>>>> zilin_api_spec
 }
 
 // Recent books
@@ -138,28 +139,27 @@ exports.book_recent_get = function (req, res) {
 exports.book_details_get = function (req, res) {
     let bookASIN = req.params.asin;
     console.log('Getting book with ASIN=' + bookASIN);
-<<<<<<< HEAD
     Book.findOne({asin: bookASIN}, function(err, book){
+        if(err){
+            console.log(err);
+            res.status(400).send({
+                success: 0,
+                error_type: 2,
+                error_msg: "Error during getting book details"
+            });
+            return;
+        }
         if(!book){
             console.log('No book found!');
             res.json({message: 'No book found!'});
         }else{
-=======
-    Book.findOne({ asin: bookASIN }, function (err, book) {
-        if (book == null) {
-            console.log('No book found!');
-            res.json('No book found!');
-        } else {
->>>>>>> zilin_api_spec
             console.log('Find book with ASIN=' + bookASIN);
             res.json(book);
         }
     });
-    // res.json(mockBooks[0]);
 }
 
 // Handle book create on POST.
-<<<<<<< HEAD
 exports.book_create_post = function(req, res, next) {
     try {
         //Check if necessary inputs are received
@@ -190,15 +190,6 @@ exports.book_create_post = function(req, res, next) {
                 error_msg: "Error during creating a new book"
             });
             return;
-=======
-exports.book_create_post = function (req, res) {
-    Book.create(req.body, function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(req.body);
-            res.json("Create Success");
->>>>>>> zilin_api_spec
         }
         console.log(req.body);
         res.json({message: "Create Success"});
