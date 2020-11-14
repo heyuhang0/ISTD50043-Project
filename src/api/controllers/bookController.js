2c98ebff4b8103444af52964083904b870434670
Jiazheng411 <644839468@qq.com>
2020-11-14 21:29:22 +0800

    add more filter for trending books
    title, author, img, rank all there

--- src/api/controllers/bookController.js
+++ src/api/controllers/bookController.js
@@ -13,20 +13,20 @@ exports.book_search_get = function (req, res) {
     let offset = Number(req.query.offset) || 0;
     var ASINJson;
     console.log(
-      'Searching for books with',
-      'keyword=' + keyword,
-      'limit=' + limit,
-      'offset=' + offset);
-    
+        'Searching for books with',
+        'keyword=' + keyword,
+        'limit=' + limit,
+        'offset=' + offset);
+
     // > 100 per page
     if (limit > 100) {
-    res.status(400)
-        .send({
-            success: 0,
-            error_type: 1,
-            error_msg: "limit must less than 100"
-        });
-    return;
+        res.status(400)
+            .send({
+                success: 0,
+                error_type: 1,
+                error_msg: "limit must less than 100"
+            });
+        return;
     };
 
     // > 1000 records
@@ -42,16 +42,16 @@ exports.book_search_get = function (req, res) {
         return;
     };
 
-    if(!keyword){
-        res.json({message: "Please enter your keywords!"});
+    if (!keyword) {
+        res.json({ message: "Please enter your keywords!" });
         console.log("Empty keyword!");
         return;
     }
     // 1. match asin; 2. title/author
     // check if asin and get with asin
-    if(asin_regex.test(keyword)){
-        Book.findOne({"asin":keyword}, function(err, book){
-            if(err){
+    if (asin_regex.test(keyword)) {
+        Book.findOne({ "asin": keyword }, function (err, book) {
+            if (err) {
                 res.status(400).send({
                     success: 0,
                     error_type: 3,
@@ -59,7 +59,7 @@ exports.book_search_get = function (req, res) {
                 });
                 return;
             }
-            if(book){
+            if (book) {
                 console.log("1 result found with ASIN!");
             }
             ASINJson = book;
@@ -67,15 +67,17 @@ exports.book_search_get = function (req, res) {
     }
     var list_of_keyword = keyword.split(" ");
     var keywords_for_find = new Array();
-    list_of_keyword.forEach(function(value, index, array){
-        if(value!==""){
-            keywords_for_find.push({"$or": [{'author':{"$regex":" "+value, "$options": "i"}}, {'author':{"$regex":value+" ", "$options": "i"}},
-         {'title': {"$regex": " "+value, "$options": "i"}}, {'title': {"$regex": value+" ", "$options": "i"}}]});
+    list_of_keyword.forEach(function (value, index, array) {
+        if (value !== "") {
+            keywords_for_find.push({
+                "$or": [{ 'author': { "$regex": " " + value, "$options": "i" } }, { 'author': { "$regex": value + " ", "$options": "i" } },
+                { 'title': { "$regex": " " + value, "$options": "i" } }, { 'title': { "$regex": value + " ", "$options": "i" } }]
+            });
         }
     });
     // get with title/author
-    Book.find({"$and": keywords_for_find}).limit(limit).skip(limit*offset).exec(function(err, books){
-        if(err){
+    Book.find({ "$and": keywords_for_find }).limit(limit).skip(limit * offset).exec(function (err, books) {
+        if (err) {
             res.status(400).send({
                 success: 0,
                 error_type: 3,
@@ -84,15 +86,15 @@ exports.book_search_get = function (req, res) {
             return;
         }
         console.log(books.length + " result(s) found apart from ASIN!");
-        if(ASINJson){
+        if (ASINJson) {
             books.push(ASINJson);
         }
-        if(books.length>0){  
+        if (books.length > 0) {
             res.json(books);
-        }else{
-            res.json({message: "No result found!"});
+        } else {
+            res.json({ message: "No result found!" });
         }
-        
+
     });
     // res.json(mockBooks);
 };
@@ -102,7 +104,7 @@ exports.book_find_by_price = function (req, res) {
     var price = parseFloat(req.query.price);
     var q = Book.find({ 'price': price }).limit(5);
     q.exec(function (err, book) {
-        if(err){
+        if (err) {
             res.status(400).send({
                 success: 0,
                 error_type: 3,
@@ -116,31 +118,42 @@ exports.book_find_by_price = function (req, res) {
 }
 
 // Trending books
-exports.book_trending_get = function(req,res) {
-    Book.find({}).limit(5).sort([["review_number", -1], ["rating_average", -1]]).exec(function(err, books){
-        if(err){
-            res.status(400).send({
-                success: 0,
-                error_type: 2,
-                error_msg: "Error during finding trending books"
-            });
-            return;
-        }
-        res.json(books);
-    });
+exports.book_trending_get = function (req, res) {
+    Book.find({
+        "title": {"$nin": [""]},
+        "author": {"$nin": [""]},
+        "category": {"$nin": [""]},
+        "rating_average": {"$nin": [""]},
+        "imUrl": {"$nin": [""]}
+    })
+        .sort([["review_number", -1], ["rating_average", -1]])
+        .limit(10)
+        .exec(function (err, books) {
+            if (err) {
+                res.status(400).send({
+                    success: 0,
+                    error_type: 2,
+                    error_msg: "Error during finding trending books"
+                });
+                return;
+            }
+            res.json({
+                success: 1,
+                books: books});
+        });
 }
 
 // Recent books
 exports.book_recent_get = function (req, res) {
-    res.json({ 'books': mockBooks.reverse() });
+    res.json({ success: 1, books: mockBooks.reverse() });
 }
 
 // Get book on asin
 exports.book_details_get = function (req, res) {
     let bookASIN = req.params.asin;
     console.log('Getting book with ASIN=' + bookASIN);
-    Book.findOne({asin: bookASIN}, function(err, book){
-        if(err){
+    Book.findOne({ asin: bookASIN }, function (err, book) {
+        if (err) {
             console.log(err);
             res.status(400).send({
                 success: 0,
@@ -149,10 +162,10 @@ exports.book_details_get = function (req, res) {
             });
             return;
         }
-        if(!book){
+        if (!book) {
             console.log('No book found!');
-            res.json({message: 'No book found!'});
-        }else{
+            res.json({ message: 'No book found!' });
+        } else {
             console.log('Find book with ASIN=' + bookASIN);
             res.json(book);
         }
@@ -160,29 +173,29 @@ exports.book_details_get = function (req, res) {
 }
 
 // Handle book create on POST.
-exports.book_create_post = function(req, res, next) {
+exports.book_create_post = function (req, res, next) {
     try {
         //Check if necessary inputs are received
         if (!req.query.title || typeof req.body.title !== "string") {
             throw new Error("POST Request Needs 'title' String Parameter");
         }
-        if(!req.query.author || typeof req.body.author !== "string"){
+        if (!req.query.author || typeof req.body.author !== "string") {
             throw new Error("POST Request Needs 'author' String Parameter");
         }
-        if(!req.query.price || typeof req.body.price !== "number"){
+        if (!req.query.price || typeof req.body.price !== "number") {
             throw new Error("POST Request Needs 'price' Number Parameter");
         }
-        if(!req.query.categories || typeof req.body.categories !== "string"){
+        if (!req.query.categories || typeof req.body.categories !== "string") {
             throw new Error("POST Request Needs 'categories' String Parameter");
         }
-        if(!req.query.description || typeof req.body.description !== "string"){
+        if (!req.query.description || typeof req.body.description !== "string") {
             throw new Error("POST Request Needs 'description' String Parameter");
         }
     } catch (e) {
         return next(e);
     }
-    Book.create(req.body, function(err){
-        if(err){
+    Book.create(req.body, function (err) {
+        if (err) {
             console.log(err);
             res.status(400).send({
                 success: 0,
@@ -192,7 +205,7 @@ exports.book_create_post = function(req, res, next) {
             return;
         }
         console.log(req.body);
-        res.json({message: "Create Success"});
+        res.json({ message: "Create Success" });
     });
     // res.send('NOT IMPLEMENTED: Book create POST');
 };
