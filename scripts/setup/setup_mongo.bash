b839816b35cb4f4eb569c1037a6b9f0c00498793
heyuhang0 <heyuhang0@outlook.com>
2020-11-22 04:10:18 +0800

    Deploy concurrently

--- scripts/setup/setup_mongo.bash
+++ scripts/setup/setup_mongo.bash
@@ -13,8 +13,8 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $VERSION_CODENAME/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 # Install MongoDB
-apt update
-apt install -y mongodb-org unzip
+apt update -qq 
+apt install -qq -y mongodb-org unzip
 
 # Start MongoDB
 echo "Launching MongoDB..."
@@ -22,11 +22,8 @@ sudo systemctl restart mongod
 while [ 1 ]; do echo "Waitting for MongoDB to start..."; echo "exit" | mongo --quiet > /dev/null && break; sleep 1; done
 
 # Create administrator
-export MONGO_HOST=$(curl -s http://checkip.amazonaws.com)
-export MONGO_ADMIN_USR="MongoAdmin"
-export MONGO_ADMIN_PWD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-16}; echo;)
-export MONGO_ADMIN_URL="mongodb://$MONGO_ADMIN_USR:$MONGO_ADMIN_PWD@$MONGO_HOST:27017"
-echo $MONGO_ADMIN_URL >> mongodb_urls
+MONGO_ADMIN_USR="MongoAdmin"
+MONGO_ADMIN_PWD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-16}; echo;)
 
 RUNMONGO () {
   echo "$*" | mongo --quiet
@@ -44,11 +41,10 @@ RUNMONGO "
 "
 
 # Create database and user
-export MONGO_DB="DBProject"
-export MONGO_USR="$(echo $MONGO_DB)User"
-export MONGO_PWD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-16}; echo;)
-export MONGO_URL="mongodb://$MONGO_USR:$MONGO_PWD@$MONGO_HOST:27017/$MONGO_DB?authSource=$MONGO_DB"
-echo $MONGO_URL >> mongodb_urls
+MONGO_DB="DBProject"
+MONGO_USR="$(echo $MONGO_DB)User"
+MONGO_PWD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-16}; echo;)
+MONGO_LOCAL_URL="mongodb://$MONGO_USR:$MONGO_PWD@$localhost:27017/$MONGO_DB?authSource=$MONGO_DB"
 
 RUNMONGO "
   use $MONGO_DB
@@ -76,15 +72,22 @@ wget -q -c https://istd50043-assets.s3-ap-southeast-1.amazonaws.com/kindle_metad
 unzip kindle_metadata_cleaned.zip
 for filename in ./combined_output/part-*; do
   echo "Importing $filename"
-  mongoimport --quiet --collection books $MONGO_URL $filename
+  mongoimport --quiet --collection books $MONGO_LOCAL_URL $filename
 done
 rm -rf kindle_metadata_cleaned.zip
 rm -rf combined_output
 
 wget -q -c https://istd50043-assets.s3-ap-southeast-1.amazonaws.com/kindle_categories.json
-mongoimport --quiet --collection categories $MONGO_URL kindle_categories.json
+mongoimport --quiet --collection categories $MONGO_LOCAL_URL kindle_categories.json
 rm -rf kindle_categories.json
 
 # Finsih setup
-echo "Done!"
-echo "You can now access: $MONGO_URL"
+echo "Finished setting up MongoDB."
+echo "\
+export MONGO_ADMIN_USR=$MONGO_ADMIN_USR
+export MONGO_ADMIN_PWD=$MONGO_ADMIN_PWD
+export MONGO_DB=$MONGO_DB
+export MONGO_USR=$MONGO_USR
+export MONGO_PWD=$MONGO_PWD
+" > ~/.credentials
+echo "Credentials have been saved to ~/.credentials"
--- scripts/setup/setup_web.bash
+++ scripts/setup/setup_web.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check Root
 if [ `id -u` -ne 0 ]; then
 	echo Need sudo
 	exit 1
@@ -7,6 +8,26 @@ fi
 
 set -e
 
-apt update -y && apt install -y python3 nodejs npm nginx
+# Installl nodejs and nginx
+apt update -qq -y && apt install -qq -y nodejs npm nginx
 
+# Install pm2
 npm install -g pm2 --loglevel warn
+
+# Setup Nginx forward
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html;
+    server_name _;
+    location / {
+        try_files \$uri \$uri/ /index.html;
+    }
+    location /api {
+        proxy_pass http://127.0.0.1:8080;
+    }
+}" > /etc/nginx/sites-available/default
+nginx -s stop || true
+nginx
+nginx -s reload
