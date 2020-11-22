dd76c4d3d834c677f8df32651f283facbc9ffbc9
Jiaxi Li <li.jiaxi97@gmail.com>
2020-11-22 22:43:19 +0800

    Add search page components

---
+++ src/pages/search/components/BooksCard/BooksCard.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Card, List, Rate } from 'antd';
+import axios from 'axios';
+import './BooksCard.less';
+
+
+function BookItem(props) {
+    return (
+        <List.Item>
+            <div className="book-item">
+                <div className="book-cover">
+                    <img src={props.book.imUrl} alt={props.book.title} />
+                </div>
+                <div className="book-content">
+                    <a className="title" href={"/book/" + escape(props.book.asin)}>
+                        <h4>{props.book.title}</h4>
+                    </a>
+                    <p className="author">
+                        by <a href={"/search?q=" + escape(props.book.author)}>{props.book.author}</a>
+                    </p>
+                    <Rate
+                        allowHalf
+                        disabled
+                        defaultValue={props.book.rating_average}
+                    />
+                    <span className="rate-num"> {props.book.rating_average.toFixed(1)}</span>
+                </div>
+            </div>
+        </List.Item>
+    );
+}
+
+class BooksCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [],
+            loading: true
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/api/books/search', {
+            params: {
+                keyword: this.props.url.split("=").pop(),
+                limit: 0,
+                offset: 0
+            }
+        }).then((res) => {
+            console.log("search get")
+            console.log(this.props.url)
+            this.setState({
+                books: res.data,
+                loading: false
+            });
+        });
+    }
+
+    render() {
+        return (
+            <Card className="books-card" title={this.props.title}>
+                <List
+                    dataSource={this.state.books}
+                    loading={this.state.loading}
+                    renderItem={book => <BookItem book={book} />}
+                />
+            </Card>
+        )
+    }
+}
+
+export default BooksCard;
+
