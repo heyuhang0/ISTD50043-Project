d51def0416d061791202c436772fcadad729f60d
heyuhang0 <heyuhang0@outlook.com>
2020-11-24 08:45:33 +0800

    Responsive design for book page

--- src/pages/book/book.js
+++ src/pages/book/book.js
@@ -18,7 +18,7 @@ function Book(props) {
       {/* <h1>Book Page for Book {asin}</h1> */}
       <Content className="book-content">
         <Row className="book-row">
-          <Col span={17}>
+          <Col xs={24} sm={24} lg={17}>
             <BookCard url={`/api/books/${asin}`} />
             {/* <Card title="Start your review here">
               <AddReview title="Start your review" asin={`${asin}`} />
@@ -27,7 +27,7 @@ function Book(props) {
               <ReviewComment url={`/api/books/${asin}/reviews`} perPage={10} />
             </Card>
           </Col>
-          <Col span={7}>
+          <Col xs={24} sm={24} lg={7}>
             <RecommendationCard title="People also viewed" url={`/api/books/${asin}`} />
           </Col>
         </Row>
